9366f7c4fd2ef6312b267505689303a1eac49303
Fajar Rizki Aditiya <you@example.com>
2024-06-12 03:30:24 +0700

    first commit

---
+++ koneksi.php
@@ -0,0 +1,16 @@
+<?php
+
+$hostname = "localhost";
+$userDataBase = "root";
+$passwordUser = "";
+$databaseName = "kuliah_pemrograman_web";
+
+$koneksi = mysqli_connect($hostname, $userDataBase, $passwordUser, $databaseName) or die(mysqli_error());
+
+//if ($koneksi) {
+  //  echo "berhasil koneksi";
+//} else {
+ //   echo "gagal koneksi";
+//}
+
+?>
